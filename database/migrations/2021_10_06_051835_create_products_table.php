d47600508d066c198416b6cb204f5c4b921ccb05
Kafriansyah <kafri@prevalensi.com>
2021-10-06 13:42:56 +0800

    product

---
+++ database/migrations/2021_10_06_051835_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->tinyInteger('stock');
+            $table->integer('price');
+            $table->date('harvest_time');
+            $table->text('description');
+            $table->tinyInteger('discount');
+            $table->bigInteger('product_group_id')->unsigned();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+
+            $table->foreign('product_group_id')->references('id')->on('product_groups')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
