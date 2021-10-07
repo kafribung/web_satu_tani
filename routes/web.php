8ac20df02d556465574a1bcda359d28cde6ba2e6
Kafriansyah <kafri@prevalensi.com>
2021-10-07 14:17:55 +0800

    mail trap

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'client.home.home');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
