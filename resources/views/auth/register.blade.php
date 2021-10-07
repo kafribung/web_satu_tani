310afc72e41324517045cf2e348573ff193ee826
Kafriansyah <kafri@prevalensi.com>
2021-10-07 11:14:46 +0800

    register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,105 @@
-@extends('layouts.app')
+<x-main>
+    <!--====== Register ======-->
+    <div class="u-s-p-b-60">
+        <div class="section__content">
+            <div class="container">
+                <div class="row row--center">
+                    <div class="col-lg-6 col-md-8 u-s-m-b-30">
+                        <div class="l-f-o">
+                            <div class="l-f-o__pad-box u-s-m-b-60">
+                                <div class="section__intro u-s-m-b-60">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <div class="section__text-wrap">
+                                                    <h1 class="section__heading u-c-secondary">PENDAFTARAN</h1>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                                <div class="l-f-o__form">
+                                    <form method="POST" action="{{ route('register') }}">
+                                        @csrf
+                                        <div class="u-s-m-b-30">
+                                            <label class="gl-label" for="reg-lname">NAMA LENGKAP *</label>
+                                            <input name="name" class="input-text input-text--primary-style" type="text"
+                                                id="reg-lname" placeholder="Nama Lengkap">
+                                            @error('name')
+                                            <p class="alert alert-danger">{{ $message }}</p>
+                                            @enderror
+                                        </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                                        <div class="u-s-m-b-30">
+                                            <span class="gl-label" for="reg-birthday">TANGGAL LAHIR *</span>
+                                            <input name="date_birth" class="input-text input-text--primary-style" type="date"
+                                                id="reg-birthday">
+                                            @error('date_birth')
+                                            <p class="alert alert-danger">{{ $message }}</p>
+                                            @enderror
+                                        </div>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                                        <div class="u-s-m-b-30">
+                                            <label class="gl-label" for="gender">JENIS KELAMIN *</label>
+                                            <select name="gender"
+                                                class="select-box select-box--primary-style u-w-100"
+                                                id="gender">
+                                                <option selected>Pilih</option>
+                                                <option value="pria">Laki-laki</option>
+                                                <option value="wanita">Perempuan</option>
+                                            </select>
+                                            @error('gender')
+                                            <p class="alert alert-danger">{{ $message }}</p>
+                                            @enderror
+                                        </div>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                        <div class="u-s-m-b-30">
+                                            <label class="gl-label" for="reg-email">E-MAIL *</label>
+                                            <input name="email" class="input-text input-text--primary-style" type="text"
+                                                id="reg-email" placeholder="Contoh: mail@satutani.com">
+                                            @error('gender')
+                                            <p class="alert alert-danger">{{ $message }}</p>
+                                            @enderror
+                                        </div>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                        <div class="u-s-m-b-30">
+                                            <label class="gl-label" for="reg-password">KATA SANDI *</label>
+                                            <input name="password" class="input-text input-text--primary-style" type="password"
+                                                id="reg-password" placeholder="Masukkan Kata Sandi">
+                                            @error('password')
+                                            <p class="alert alert-danger">{{ $message }}</p>
+                                            @enderror
+                                        </div>
+
+                                        <div class="u-s-m-b-30">
+                                            <label class="gl-label" for="password_confirmation">KATA SANDI *</label>
+                                            <input name="password_confirmation" class="input-text input-text--primary-style" type="password"
+                                                id="password_confirmation" placeholder="Konfirmasi Kata Sandi">
+                                            @error('password_confirmation')
+                                            <p class="alert alert-danger">{{ $message }}</p>
+                                            @enderror
+
+                                            <br><br>
+                                            <span>Klik</span>
+                                            <a href="{{ route('login') }}" style="color:#01AA4F;">masuk</a>
+                                            <span>jika anda sudah memiliki akun</span>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-xl-3 col-lg-2 col-md-2 col-sm-2 u-s-m-b-30">
+                                                <button class="btn btn--e-brand-b-2"
+                                                    type="submit">Lanjutkan</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    <!--====== End - Register ======-->
+</x-main>
