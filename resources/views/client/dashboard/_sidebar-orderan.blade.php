8f4d4cba43aa63515569584bf0a2a508cf84e4a8
kafribung <60900117001@uin-alauddin.ac.id>
2021-11-06 17:20:38 +0800

    orderan

---
+++ resources/views/client/dashboard/_sidebar-orderan.blade.php
@@ -0,0 +1,50 @@
+<div class="dash__box dash__box--shadow dash__box--radius dash__box--bg-white u-s-m-b-30">
+    <div class="dash__pad-2">
+        <div class="manage-o">
+            <div class="manage-o__timeline">
+                <div class="timeline-row">
+                    <div class="col-lg-3 u-s-m-b-30">
+                        <a href="{{ route('pesanan.index') }}">
+                            <div class="timeline-step">
+                                <div class="timeline-l-i timeline-l-i--finish">
+                                    <span class="timeline-circle"></span>
+                                </div>
+                                <span class="timeline-text">Menunggu Pembayaran</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 u-s-m-b-30">
+                        <a href="pesanan-diproses.html">
+                            <div class="timeline-step">
+                                <div class="timeline-l-i">
+                                    <span class="timeline-circle"></span>
+                                </div>
+                                <span class="timeline-text">Diproses</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 u-s-m-b-30">
+                        <a href="pesanan-dikirim.html">
+                            <div class="timeline-step">
+                                <div class="timeline-l-i">
+                                    <span class="timeline-circle"></span>
+                                </div>
+                                <span class="timeline-text">Dikirim</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 u-s-m-b-30">
+                        <a href="pesanan-selesai.html">
+                            <div class="timeline-step">
+                                <div class="timeline-l-i">
+                                    <span class="timeline-circle"></span>
+                                </div>
+                                <span class="timeline-text">Selesai</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
