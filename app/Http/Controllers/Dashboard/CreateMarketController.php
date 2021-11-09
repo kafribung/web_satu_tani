b832d5051862cc74eb0342b7c2ee766b213ed7df
kafribung <60900117001@uin-alauddin.ac.id>
2021-11-09 19:46:30 +0800

    perbaiki pendaftaran

--- app/Http/Controllers/Dashboard/CreateMarketController.php
+++ app/Http/Controllers/Dashboard/CreateMarketController.php
@@ -12,7 +12,7 @@ class CreateMarketController extends Controller
     {
         // Jika sebelumnya mengisi form
         if (auth()->user()->validation_sellers()->count() > 0) {
-            if (auth()->user()->validation_sellers->name == 'Koperasi') {
+            if (auth()->user()->validation_sellers->name == 'Koperasi' && auth()->user()->validation_sellers->bank == null && auth()->user()->validation_sellers->rekening_number == null && auth()->user()->validation_sellers->rekening_name == null) {
                 return redirect()->route('buat-toko.koperasi');
             } else return redirect()->route('buat-toko.petani');
         }
--- app/Http/Controllers/Dashboard/CreateMarketCooperative2Controller.php
+++ app/Http/Controllers/Dashboard/CreateMarketCooperative2Controller.php
@@ -25,10 +25,11 @@ class CreateMarketCooperative2Controller extends Controller
             return redirect()->route('buat-toko.koperasi');
         }
 
+        // Jika sudah mendaftar menjadi seller
         if (auth()->user()->validation_sellers()->count() > 0) {
-            if (auth()->user()->validation_sellers->name == "Koperasi" && auth()->user()->validation_sellers->bank == null && auth()->user()->validation_sellers->rekening_number == null && auth()->user()->validation_sellers->rekening_name == null) {
+            if (auth()->user()->validation_sellers->bank != null && auth()->user()->validation_sellers->rekening_number != null && auth()->user()->validation_sellers->rekening_name != null) {
                 return redirect()->route('buat-toko');
-            } else return redirect()->route('buat-toko');
+            }
         }
 
         $user = auth()->user();
