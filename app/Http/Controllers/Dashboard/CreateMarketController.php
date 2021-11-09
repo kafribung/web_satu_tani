b2b1633af2c047536eff8fbd3c61b882242826d4
kafribung <60900117001@uin-alauddin.ac.id>
2021-11-09 18:26:02 +0800

    mantap

--- app/Http/Controllers/Dashboard/CreateMarketController.php
+++ app/Http/Controllers/Dashboard/CreateMarketController.php
@@ -11,8 +11,11 @@ class CreateMarketController extends Controller
     public function __invoke()
     {
         // Jika sebelumnya mengisi form
-        if (auth()->user()->validation_sellers->name == 'Koperasi') return redirect()->route('buat-toko.koperasi');
-        else return redirect()->route('buat-toko.petani');
+        if (auth()->user()->validation_sellers()->count() > 0) {
+            if (auth()->user()->validation_sellers->name == 'Koperasi') {
+                return redirect()->route('buat-toko.koperasi');
+            } else return redirect()->route('buat-toko.petani');
+        }
 
         return view('client.dashboard.create-market');
     }
